5174803c97964d1611fadbcb4b1eb26bc49699b5
rsgcata <rsgcata@users.noreply.github.com>
2024-07-14 22:13:01 +0300

    Added documentation to migration API

--- migration/migration.go
+++ migration/migration.go
@@ -11,19 +11,42 @@ import (
 	"time"
 )
 
+// TmplContents Migration file template to be used to generate a new, base migration file
+// to be easier for devs to create new migrations.
+//
 //go:embed migration.go.template
 var TmplContents string
 
+// FileNamePrefix Migration file name prefix, static value,
+// which will be set for all migration files.
 const FileNamePrefix = "version"
+
+// FileNameSeparator A separator used to separate words in a migration file.
 const FileNameSeparator = "_"
 
+// Migration Represents the base behavior a migration should include
 type Migration interface {
+	// Version must be a static, globally unique value which identifies the migration file
+	// globally. It must match the value set in the file name. By default, this will be
+	// the unix timestamp in seconds when the migration file was generated.
 	Version() uint64
+
+	// Up must include any code that will change the structure and/or state of your database.
+	// Preferably, it should be idempotent and resilient. It should not use, set or modify
+	// global state, to not impact other migration executions. You can have multiple migrations
+	// Up() act as a unit, but, care should be taken when coordinating them (use save points
+	// for example, and save them in a central place which can be used as a persistent
+	// source of truth).
 	Up() error
+
+	// Down must include all necessary code that will roll back the changes made by the Up()
+	// function. Care should be taken when designing your rollback strategy, to not lose any
+	// critical database state. Other things mentioned for Up() function are valid for Down()
+	// also.
 	Down() error
 }
 
-// Dummy migration struct that should be used only in tests
+// DummyMigration struct that should be used only in tests
 type DummyMigration struct {
 	version uint64
 }
@@ -44,11 +67,18 @@ type migrationTemplateData struct {
 	PackageName string
 }
 
+// MigrationsDirPath should be used, preferably, as a global, static value, to determine where
+// the migration files are placed in the file system.
 type MigrationsDirPath string
 
+// ErrCreateMigrationsDirPath is a generic error for the scenarios when the migrations
+// directory path can't be created (for example, nonexistent directory in the file system).
 var ErrCreateMigrationsDirPath = errors.New("could not create new migrations directory path")
+
+// ErrBlankMigration is a generic error for failing to create a blank migration
 var ErrBlankMigration = errors.New("could not generate blank migration")
 
+// NewMigrationsDirPath can be used to create a new MigrationsDirPath
 func NewMigrationsDirPath(dirPath string) (MigrationsDirPath, error) {
 	fileInfo, err := os.Stat(dirPath)
 
@@ -71,10 +101,10 @@ func newMigrationTemplateData(dirPath MigrationsDirPath) migrationTemplateData {
 	return migrationTemplateData{uint64(time.Now().Unix()), filepath.Base(string(dirPath))}
 }
 
-// Generates a blank migration file in the specified directory
+// GenerateBlankMigration generates a blank migration file in the specified directory
 // Returns the generated file name
 // Errors if template processing failed or file creation failed
-func GenerateBlankMigration(dirPath MigrationsDirPath) (string, error) {
+func GenerateBlankMigration(dirPath MigrationsDirPath) (fileName string, err error) {
 	tmpl, err := template.New("migration").Parse(TmplContents)
 
 	if err != nil {
@@ -84,7 +114,7 @@ func GenerateBlankMigration(dirPath MigrationsDirPath) (string, error) {
 	}
 
 	tmplData := newMigrationTemplateData(dirPath)
-	fileName := FileNamePrefix + FileNameSeparator + strconv.Itoa(int(tmplData.Version)) + ".go"
+	fileName = FileNamePrefix + FileNameSeparator + strconv.Itoa(int(tmplData.Version)) + ".go"
 	filePath := filepath.Join(string(dirPath), fileName)
 
 	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
@@ -94,14 +124,23 @@ func GenerateBlankMigration(dirPath MigrationsDirPath) (string, error) {
 			"%w, file creation failed with error: %w", ErrBlankMigration, err,
 		)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		if closeErr := file.Close(); closeErr != nil {
+			err = errors.Join(err, closeErr)
+		}
+	}(file)
 
 	if err = tmpl.Execute(file, tmplData); err != nil {
-		file.Close()
-		os.Remove(filePath)
-		return "", fmt.Errorf(
+		err = fmt.Errorf(
 			"%w, failed to generate contents with error: %w", ErrBlankMigration, err,
 		)
+		closeErr := file.Close()
+
+		if removeErr := os.Remove(filePath); closeErr != nil || removeErr != nil {
+			err = errors.Join(err, closeErr, removeErr)
+		}
+
+		return "", err
 	}
 
 	return fileName, nil
